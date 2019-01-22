e880ae85ba855ad934432558601f211d41f5bd66
Jigneshkumar Parekh <jigneshparekh.it@gmail.com>
2019-01-21 21:30:32 -0800

    Added express server on arduino board ready event

--- Microcnotrollers-connect/arduino-uno-server.js
+++ Microcnotrollers-connect/arduino-uno-server.js
@@ -1,25 +1,32 @@
 const five = require('johnny-five');
 const http = require('http');
 const url = require('url');
-
+const express = require('express');
+const app = express();
 const board = new five.Board();
+
+
+
 
 board.on(`ready`, () => {
   console.log(`--> Board is ready...`);
-  const server = http.createServer((req, res) => {
+  app.get('*', (req, res) => {
     const urlParts = url.parse(req.url, true);
     const { method } = req;
 
+    console.log(`--> URL parts: `, urlParts);
+  
     if (method === 'POST' && urlParts.pathname === '/turnOn') {
       res.end('Lamp turned on');
     } else if(method === 'POST' && urlParts.pathname === '/turnOff') {
       res.end('Lamp tunred off');
     } else {
-      res.end('Success');
+      res.end('Hello! from Arduino...');
     }
+  });
 
-    server.listen(8080, () => {
-      console.log(`--> Server is running at ${server.address()}`);
-    });
+ 
+  app.listen(8080, () => {
+    console.log(`--> Server is listening on port 8080`);
   });
 });
